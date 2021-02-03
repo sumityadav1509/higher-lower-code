97cc600c9f5f4d11293dc138ca0225d3734adf37
sumityadav1509 <sumityadav2005@gmail.com>
2021-02-03 11:39:32 +0000

    Extraction of data from game_data completed.

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ account_a=random.choice(data)
 account_b=random.choice(data) 
 if account_a==account_b:
   account_b=random.choice(data) 
-  
+
+
+account_name= account_a["name"] 
+account_descr= account_a["description"] 
+account_country= account_a["country"] 
+print(f"{account_name} , a {account_descr}  , from {account_country} ")  
+
+
+
  
 
 

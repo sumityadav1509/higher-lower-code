379ac46c54b9689b835c0c39464865c259877489
sumityadav1509 <sumityadav2005@gmail.com>
2021-02-03 10:47:14 +0000

    Initial phase of coding.

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-insta_data=[
-2                {
-3                 'name':'Justin bieber' ,
-4                 'followers':50 ,
-5                 'profession':'Artist'
-6                 },
-7                 {
-8                  'name':'Justin Timberlake' , 
-9                  'followers':70 ,
-10                  'profession':'Artist'
-11                  }
-12                ]   
-13    def compare_followers(guess,a_followers,b_followers):
-14        if a_followers>b_followers:
-15            return guess=="a" 
-16        else:
-17            return guess=="b" 
-18            
-19    a_follower= insta_data[0] 
-20    b_follower=insta_data[1] 
-      a_followers=a_follower["followers"]
-      b_followers=b_follower["followers"] 
-      guess=input("Guess 'A' or 'B').lower() 
-     compare_followers(guess,a_followers,b_followers)  
+# Display Art  
+from art import logo 
+from game_data import data 
+import random
+print(logo)   
+account_a=random.choice(data) 
+account_b=random.choice(data) 
+if account_a==account_b:
+  account_b=random.choice(data) 
+  
+ 
+
+
+
+
+# Generate a random account from game data.
+
+# Format the account data into printable format. 
+
+# Ask a user for guess
+ 
+
+# Check if user is correct 
+# Get follower count 
